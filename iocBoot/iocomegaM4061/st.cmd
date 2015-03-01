cf831669aa62fd0e89b8c80245eba67a5b4f3512
Stuart B. Wilkins <swilkins@bnl.gov>
2015-03-01 09:41:06 -0500

    ENH : First checkin of working version

--- iocBoot/iocomegaM4061/st.cmd
+++ iocBoot/iocomegaM4061/st.cmd
@@ -3,16 +3,28 @@
 ## You may have to change omegaM4061 to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
+epicsEnvSet("EPICS_CA_ADDR_LIST"      , "10.23.0.255")
 
 ## Register all support components
 dbLoadDatabase("../../dbd/omegaM4061.dbd",0,0)
 omegaM4061_registerRecordDeviceDriver(pdbbase) 
 
+# Do ASYN stuff
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/proto")
+# Port 1 on terminal server
+drvAsynIPPortConfigure("IP1", "10.23.2.62:4001")
+asynSetTraceMask("IP1", 0, 0x9)
+asynSetTraceIOMask("IP1", 0, 0x2)
+
 ## Load record instances
-dbLoadRecords("../../db/omegaM4061.db","user=swilkins")
+dbLoadRecords("$(TOP)/db/omegaM4061.db", "Sys=XF:23ID1-ES,Dev={Cryo:He},PREC=3,PORT=IP1,MAX_FLOW=10,K_FACTOR=1.454,SCAN=10 second,ADDR=11")
 
 iocInit()
 
-## Start any sequence programs
-#seq sncomegaM4061,"user=swilkins"
+dbl > $(TOP)/records.dbl
+system("cp $(TOP)/records.dbl /cf-update/xf23id1-ioc2.omegaM4061.dbl")
+
---
+++ st.cmd
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd iocBoot/iocomegaM4061 && ./st.cmd
+
