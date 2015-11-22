19cda3db8fd295da60e772f931e591d2ded3ddcd
Stuart B. Wilkins <swilkins@bnl.gov>
2015-11-22 13:22:32 -0500

    Checkin

--- iocBoot/iocomegaM4061/st.cmd
+++ iocBoot/iocomegaM4061/st.cmd
@@ -17,14 +17,20 @@ omegaM4061_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/proto")
 # Port 1 on terminal server
 drvAsynIPPortConfigure("IP1", "10.23.2.62:4001")
-asynSetTraceMask("IP1", 0, 0x9)
-asynSetTraceIOMask("IP1", 0, 0x2)
+drvAsynIPPortConfigure("IP2", "10.23.2.62:4002")
+#asynSetTraceMask("IP1", 0, 0x9)
+#asynSetTraceIOMask("IP1", 0, 0x2)
+#asynSetTraceMask("IP2", 0, 0x9)
+#asynSetTraceIOMask("IP2", 0, 0x2)
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/omegaM4061.db", "Sys=XF:23ID1-ES,Dev={Cryo:He},PREC=3,PORT=IP1,MAX_FLOW=10,K_FACTOR=1.454,SCAN=10 second,ADDR=11")
+#dbLoadRecords("$(TOP)/db/omegaM4061.db", "Sys=XF:23ID1-ES,Dev={Cryo:He},PREC=3,PORT=IP1,K_FACTOR=1.454,SCAN=.1 second,ADDR=11")
+dbLoadRecords("$(TOP)/db/omegaM4061.db", "Sys=XF:23ID1-ES,Dev={FCCD-N2},PREC=3,PORT=IP2,K_FACTOR=1.0,SCAN=.1 second,ADDR=11")
+
+dbLoadRecords("$(TOP)/db/iocAdminSoft.db", "IOC=XF:23ID1-CT{IOC:OmegaM4061}")
 
 iocInit()
 
 dbl > $(TOP)/records.dbl
-system("cp $(TOP)/records.dbl /cf-update/xf23id1-ioc2.omegaM4061.dbl")
+system("cp $(TOP)/records.dbl /cf-update/xf23id1-ioc3.omegaM4061.dbl")
 
